db66c0bd9ea7bb29300e8800c7eb14795c92c045
M. Khaled (MBP) <eng.mk@msn.com>
2020-05-23 01:34:24 +0200

    mods

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 (UNDER DEVELOPMENT) Since etoro-api is still not complete and being updated constantly, i am creating this interface as an easy-to-maintain python REST API for etoro. It uses the OpenAPI specifications  generated from the etoro API website and generates a generic python wrapper to use them. Any update to the API from the etoro side can be reflected here by updatin gthe api from the provided OpenAPI descriptions provided by eTroo.
 
 
-You need to have openapi-generatpr installed.
-Here you can find how to installl it: https://github.com/OpenAPITools/openapi-generator
+You need to have the swagger code-geenratorr installed.
+Here you can find how to installl it: https://github.com/swagger-api/swagger-codegen
 
 
 run the script update.sh to update the api too its lastest specifications and to generate the python client.
--- update.sh
+++ update.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
 # remeove old api
-rm -rf api
-mkdir api
+rm -rf api*
+
+
+# sources of the api as OpenAPI files
+API_DISCOVERY="./specs/Discovery.json"
+API_METADATA="./specs/Metadata.json"
+API_RATES="./specs/Rates.json"
+API_SYSTEM="./specs/System.json"
+API_TRADING="./specs/Trading.json"
+API_USER="./specs/User.json"
 
 
 # generate the api
-openapi-generator generate -i "$OPEN_API_SCRIP" -g ruby -o ./api
-
+openapi-generator generate -i "$API_DISCOVERY" -g python -o ./api_Discovery
+openapi-generator generate -i "$API_METADATA" -g python -o ./api_Metadata
+openapi-generator generate -i "$API_RATES" -g python -o ./api_Rates
+openapi-generator generate -i "$API_SYSTEM" -g python -o ./api_System
+openapi-generator generate -i "$API_TRADING" -g python -o ./api_Trading
+openapi-generator generate -i "$API_USER" -g python -o ./api_User
