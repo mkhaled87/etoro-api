fe9bbcb2ce45bf7695b5a0885d5fbc85c4c122cc
M. Khaled (MBP) <eng.mk@msn.com>
2020-05-23 02:30:22 +0200

    mods

--- update.sh
+++ update.sh
@@ -3,20 +3,13 @@
 # remeove old api
 rm -rf api*
 
-
-# sources of the api as OpenAPI files
-API_DISCOVERY="./specs/Discovery.json"
-API_METADATA="./specs/Metadata.json"
-API_RATES="./specs/Rates.json"
-API_SYSTEM="./specs/System.json"
-API_TRADING="./specs/Trading.json"
-API_USER="./specs/User.json"
+# the apis
+API_files=( Discovery Metadata Rates System Trading User )
+API_spec=./specs
 
 
 # generate the api
-openapi-generator generate -i "$API_DISCOVERY" -g python -o ./api_Discovery
-openapi-generator generate -i "$API_METADATA" -g python -o ./api_Metadata
-openapi-generator generate -i "$API_RATES" -g python -o ./api_Rates
-openapi-generator generate -i "$API_SYSTEM" -g python -o ./api_System
-openapi-generator generate -i "$API_TRADING" -g python -o ./api_Trading
-openapi-generator generate -i "$API_USER" -g python -o ./api_User
+for i in "${API_files[@]}"
+do
+	swagger-codegen generate -i $API_spec/$i.json -l python -o ./api_$i
+done
